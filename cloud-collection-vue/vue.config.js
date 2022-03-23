fda22031df765d3fe625897445307c6f8d48a8fe
wangbingyi <1041493283@qq.com>
2022-03-23 16:03:27 +0800

    【前端】 vue-cli使用代理转发解决开发环境下接口请求的浏览器跨域问题

--- cloud-collection-vue/vue.config.js
+++ cloud-collection-vue/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  devServer: {
+    port: 9090, // 前端开发服务器运行的端口
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:9191', // 真实的后端地址
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
 });
