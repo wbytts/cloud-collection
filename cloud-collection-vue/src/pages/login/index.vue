3c02d7fc07640a4c9793eb47f5ad180296d697a5
c22b_h1v74hfhcc <1702251811@qq.com>
2022-03-23 21:07:09 +0800

    【前端】登陆页面样式

--- cloud-collection-vue/src/pages/login/index.vue
+++ cloud-collection-vue/src/pages/login/index.vue
@@ -4,23 +4,34 @@
       <div class="head"></div>
       <div class="main">
         <el-form :model="form" :rules="rules">
-          <el-form-item prop="username" label="用户名" label-width="100px">
-            <el-input v-model="form.username"></el-input>
+          <el-form-item prop="username">
+            <el-input v-model="form.username" class="login-input"></el-input>
+            <template #label>
+              <div class="login-form-label">用户名</div>
+            </template>
           </el-form-item>
-          <el-form-item prop="password" label="密码" label-width="100px">
-            <el-input v-model="form.password" type="password"></el-input>
+          <el-form-item prop="password">
+            <el-input
+              v-model="form.password"
+              type="password"
+              class="login-input"
+            ></el-input>
+            <template #label>
+              <div class="login-form-label">密码</div>
+            </template>
           </el-form-item>
         </el-form>
       </div>
       <div class="footer">
         <el-button type="primary" @click="doLogin">登录</el-button>
+        <el-button type="primary" size="mini">注册</el-button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import testApi from '@/api/test.js';
+import testApi from "@/api/test.js";
 
 export default {
   data() {
@@ -40,10 +51,10 @@ export default {
   methods: {
     async doLogin() {
       console.log("点击了登录按钮", this.form);
-      console.log('测试发请求');
+      console.log("测试发请求");
 
       let res = await testApi.hello();
-      console.log('接口返回的结果', res);
+      console.log("接口返回的结果", res);
 
       // let testPost = await testApi.testPost();
       // console.log('测试post请求', testPost);
@@ -53,28 +64,60 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.login-container {
-  .el-input {
-    width: 300px;
-  }
-}
-
 .login-page {
-  width: 30%;
-  height: 250px;
   margin: 200px auto;
   position: relative;
-  border: 1px solid red;
-}
 
-.login-container {
-  position: relative;
-}
-
-.footer {
-  width: 200px;
+  .head {
+    display: inline-block;
+    text-align: justify;
+  }
+  .login-container {
+    width: 500px;
+    height: 250px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    box-shadow: 1px 2px 2px 4px #c7c3c3 ;
+    .main {
+      width: 300px;
+      margin: 0 auto;
+      ::v-deep .el-form-item__label {
+        display: inline-block;
+        text-align: justify;
+      }
+      .login-form-label {
+        display: inline-block;
+        width: 4em; // 初始给四个字的宽度
+        text-align: justify;
+        i {
+          display: inline-block;
+          width: 100%;
+        }
+      }
+      .login-input {
+        display: inline-block;
+        width: 200px;
+      }
+    }
+  }
+  .footer {
+    position: relative;
+    button:nth-child(1) {
+      position: absolute;
+      top: 20px;
+      left: 30px;
+      width: 40%;
+    }
+    button:nth-child(2) {
+      position: absolute;
+      top: 25px;
+      right: 30px;
+    }
+  }
 }
 </style>
-
 
 
