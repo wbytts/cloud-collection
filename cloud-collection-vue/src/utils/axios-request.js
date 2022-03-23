6a7540538fd673d8d330a6842551dc24bbc384b1
wangbingyi <1041493283@qq.com>
2022-03-23 15:43:25 +0800

    【前端】 axios的简单封装，命名为service，并增加请求拦截器和响应拦截器。创建 api 目录，用来存放请求后端接口的各种方法

---
+++ cloud-collection-vue/src/utils/axios-request.js
@@ -0,0 +1,30 @@
+﻿import axios from 'axios';
+
+// 创建通用的axios对象，发送请求
+const service = axios.create({
+  baseURL: '/api',
+  timeout: 5000, // 请求超时时间，毫秒
+});
+
+// 设置 service 的请求拦截器
+service.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 设置 service 的响应拦截器
+service.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 默认导出 service
+export default service;
