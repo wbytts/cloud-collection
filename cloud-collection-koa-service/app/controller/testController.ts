fab731a5bf500c5ba73626d33c0d6a275e558969
刘hao <18201055338@163.com>
2022-04-07 22:23:41 +0800

    update

--- cloud-collection-koa-service/app/controller/testController.ts
+++ cloud-collection-koa-service/app/controller/testController.ts
@@ -1,11 +1,16 @@
 ﻿import { Context } from 'koa';
 import responseUtil from '../utils/responseUtil';
 import logger from '../logger';
+import User from '../model/user'
 
 class TestController {
   async index(ctx: Context) {
     logger.info('访问了测试接口！');
     responseUtil.success(ctx, { msg: '这是用来测试的接口！' });
+  }
+  async findAllUser(ctx: Context) {
+    let users = await User.findAll()
+    responseUtil.success(ctx, users);
   }
 }
 
--- cloud-collection-koa-service/app/router/index.ts
+++ cloud-collection-koa-service/app/router/index.ts
@@ -14,6 +14,7 @@ router.get('/queryUserById', userController.queryUserById)
 
 // 临时接口测试
 router.get('/test', testController.index)
+router.get('/test/allUser', testController.findAllUser)
 
 
 export default router
